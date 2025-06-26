c64a452fef9b082004989568499320904bf86e4a
DivyaReddy011 <20105cm031@gmail.com>
2025-06-26 21:20:44 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-
+import get_ai_response                                    
+import predict_disease, generate_treatment_plan         
+import display_health_analytics 
 
 
 # ---- Page Config ----
